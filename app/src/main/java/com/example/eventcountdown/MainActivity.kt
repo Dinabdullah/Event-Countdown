04c0e48388acf6edc9e701bda7b7530b8537b174
dinabdullah <dina_abdullah04@yahoo.com>
2025-04-12 13:33:55 +0200

    Add Notification Alert

--- app/src/main/java/com/example/eventcountdown/MainActivity.kt
+++ app/src/main/java/com/example/eventcountdown/MainActivity.kt
@@ -38,8 +38,11 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),100)
         createNotificationChannel()
+
     }
+
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val importance = NotificationManager.IMPORTANCE_HIGH
